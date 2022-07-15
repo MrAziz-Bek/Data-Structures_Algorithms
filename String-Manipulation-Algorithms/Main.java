6d38d85e32b1d5ced1c2678a4b9a24d4eb300a4a
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-07-15 22:39:59 +0500

    String-Manipulation Algorithms

---
+++ String-Manipulation-Algorithms/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // var count = StringUtils.countVowels("Hello World");
+        // System.out.println(count);
+        // var reversed = StringUtils.reverse("blue");
+        // System.out.println(reversed);
+        // var reversedWords = StringUtils.reverseWords("Trees are beautiful");
+        // System.out.println(reversedWords);
+        // var areRotations = StringUtils.areRotations("ABCD", "");
+        // System.out.println(areRotations);
+        // var removeDuplicates = StringUtils.removeDuplicates("Trees are beautiful");
+        // System.out.println(removeDuplicates);
+        // var getMaxOccuringChar = StringUtils.getMaxOccuringChar("Trees are beautiful");
+        // System.out.println(getMaxOccuringChar);
+        // var result = StringUtils.capitalize("   tOday   iS a GrEAT dAY   ");
+        // System.out.println(result);
+        // var result = StringUtils.areAnagrams("ABcD", "DABC");
+        // System.out.println(result);
+        var result = StringUtils.isPalindrome("MADam");
+        System.out.println(result);
+    }
+}
